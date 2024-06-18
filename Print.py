e17ce9a8bd2784df025dc3fce299bc664157f128
M0nya-0d <159374625+M0nya-0d@users.noreply.github.com>
2024-06-18 21:33:51 +0300

    Update Print.py

--- Print.py
+++ Print.py
@@ -1,6 +1,6 @@
 import time
 print "bybybyty"
 print "DYD8888888888888888888888888YDYDY"
-print "DYDYDYDY"
+print "DYDYD77777777777777777777777777YDY"
 print "byby4343422gfaTEesadfsd2"
 time.sleep(10)
