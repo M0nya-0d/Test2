863541de741ccfcd1fd15e356f8749b43128ea29
M0nya-0d <159374625+M0nya-0d@users.noreply.github.com>
2024-07-14 07:19:37 +0300

    Update job.py

--- job.py
+++ job.py
@@ -1,2 +1,21 @@
-while True:
-  execfile('Print.py')
+import os
+import schedule
+import requests
+
+def regen():
+    os.system('python test.py')
+
+def ded_kill():
+    os.system('python kill.py')
+
+def main():
+    schedule.every(2).seconds.do(regen)
+    #schedule.every(20).seconds.do(ded_kill)
+    while True:
+        schedule.run_pending()
+        #schedule.run_pending()
+
+
+
+if __name__ == '__main__':
+    main()
