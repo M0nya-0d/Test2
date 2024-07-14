220f0cc4083795d10bba1aa47177ec92ed4a70c2
M0nya-0d <159374625+M0nya-0d@users.noreply.github.com>
2024-07-14 21:01:51 +0300

    Update job.py

--- job.py
+++ job.py
@@ -1,22 +1,17 @@
 import os
-import schedule # type: ignore
+import schedule 
 import requests
 user_name = "parametr6"
 file = open('inc.txt', 'r', encoding='utf-8')
 inc = int(file.read())
 file.close()
-
-#if(inc == 1):
-#    os.system('py sborUserName.py')
-    #file.write('0')
-
-
+)
 def regen():
-    os.system('py test.py')
+    os.system('python test.py')
 
 #def ded_kill():
 #    os.system('python kill.py')
-#os.system('py sborUserName.py')
+#os.system('python sborUserName.py')
 def main():
     schedule.every(2).seconds.do(regen)
     #schedule.every(20).seconds.do(ded_kill)
@@ -29,10 +24,7 @@ def main():
             file = open('inc.txt', 'w', encoding='utf-8')
             file.write('0')
             file.close()
-            os.system('py sborUserName.py')
-
-
-
+            os.system('python sborUserName.py')
 
 if __name__ == '__main__':
     main()
