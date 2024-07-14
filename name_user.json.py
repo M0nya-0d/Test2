bd268c37239f6b854ff384edb32ac4998c30ed74
M0nya-0d <159374625+M0nya-0d@users.noreply.github.com>
2024-07-14 07:17:54 +0300

    Add files via upload

---
+++ name_user.json.py
@@ -0,0 +1,101 @@
+{
+    "parametr": [
+        {
+            "hp": 3318,
+            "rad": 63350,
+            "sost_psy": 0,
+            "arm_rad": 0,
+            "arm_psy": 3,
+            "arm_elect": 0,
+            "arm_ximiy": 0
+        }
+    ]
+"parametr1": [
+        {
+            "hp": 1111,
+            "rad": 1111,
+            "sost_psy": 11111,
+            "arm_rad": 1111,
+            "arm_psy": 11111,
+            "arm_elect": 11111,
+            "arm_ximiy": 11111
+        }
+    ]
+"parametr2": [
+        {
+            "hp": 2222,
+            "rad": 222222,
+            "sost_psy": 3333,
+            "arm_rad": 3,
+            "arm_psy": 3,
+            "arm_elect": 30,
+            "arm_ximiy": 30
+        }
+    ]
+"parametr3": [
+        {
+            "hp": 3333,
+            "rad": 3333,
+            "sost_psy": 3330,
+            "arm_rad": 330,
+            "arm_psy": 3,
+            "arm_elect": 330,
+            "arm_ximiy": 3330
+        }
+    ]
+"parametr4": [
+        {
+            "hp": 4444,
+            "rad": 44444,
+            "sost_psy": 444,
+            "arm_rad": 40,
+            "arm_psy": 443,
+            "arm_elect": 4440,
+            "arm_ximiy": 4440
+        }
+    ]
+"parametr5": [
+        {
+            "hp": 5555,
+            "rad": 55555,
+            "sost_psy": 550,
+            "arm_rad": 50,
+            "arm_psy": 5553,
+            "arm_elect": 550,
+            "arm_ximiy": 550
+        }
+    ]
+"parametr6": [
+        {
+            "hp": 6666,
+            "rad": 666,
+            "sost_psy": 6660,
+            "arm_rad": 660,
+            "arm_psy": 6663,
+            "arm_elect": 6660,
+            "arm_ximiy": 6660
+        }
+    ]
+"parametr7": [
+        {
+            "hp": 77777,
+            "rad": 7777,
+            "sost_psy": 7770,
+            "arm_rad": 7770,
+            "arm_psy": 7777,
+            "arm_elect": 7770,
+            "arm_ximiy": 77770
+        }
+    ]
+"parametr8": [
+        {
+            "hp": 38888,
+            "rad": 88888,
+            "sost_psy": 888,
+            "arm_rad": 88888,
+            "arm_psy": 888,
+            "arm_elect": 8880,
+            "arm_ximiy": 88888
+        }
+    ]
+}
