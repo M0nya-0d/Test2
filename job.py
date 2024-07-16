2f50ac9dfb73cb9a30f078d582b5d442920752b9
M0nya-0d <odessatehnolink@gmail.com>
2024-07-16 15:42:57 +0300

    new8

--- job.py
+++ job.py
@@ -1,5 +1,11 @@
 #Job
 import os
-while True:
-  os.system('python3 start.py')
-  
+#while True:
+#  os.system('python3 start.py')
+def mainn():
+    os.system('python3 start.py')
+    while True:
+        mainn()
+
+if __name__ == '__main__':
+    mainn()
