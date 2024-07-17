e253e7f0a294b5ee2307457c699c097e507acfab
M0nya-0d <159374625+M0nya-0d@users.noreply.github.com>
2024-07-17 11:35:07 +0300

    Update job.py

--- job.py
+++ job.py
@@ -4,10 +4,10 @@ import os
 while True:
   #/usr/bin/python3 start.py
   #exec(open('start.py').read())
-#def mainn():
-#    os.system('python3 start.py')
-#    while True:
-#        mainn()
-#
-#if __name__ == '__main__':
-#    mainn()
+def mainn():
+    os.system('python3 start.py')
+    #while True:
+    #    mainn()
+
+if __name__ == '__main__':
+    mainn()
